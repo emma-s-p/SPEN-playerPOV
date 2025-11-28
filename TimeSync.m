80b26be6be54f82b0dbdb76e85bb4bdbb4ed265e
emma-s-p <mpj946@usask.ca>
2025-11-27 18:34:48 -0600

    Update TimeSync.m
    THIS IS NOW THE RIGHT FILE

--- TimeSync.m
+++ TimeSync.m
@@ -1,14 +1,24 @@
-function [T0] = TimeSync(audio1, audio2)
+function [T0,A1_L,A2_L] = TimeSync(audio1, audio2)
 %TimeSync finds the time stamp in the longer video that the audio syncs up.
-error = 10E-3; %error test
 
 %get the audio files in a readable format
 [a1, a1_Fs] = audioread(audio1);
 [a2,a2_Fs] = audioread(audio2);
 N1  = length(a1);
+A1_L = N1/a1_Fs;
 N2 = length(a2);
+A2_L = N2/a2_Fs;
 
-%WIND FILTER MAY NEED TO BE ADDED HERE
+%Normalize both of the audiofiles to each other
+
+min_val = min([a1;a2]);
+max_val = max([a1;a2]);
+
+r = max_val-min_val;
+a1(:,1)  = (a1(:,1)-min_val)/r;
+a2(:,1) = (a2(:,1)-min_val)/r;
+a1(:,2)  = (a1(:,2)-min_val)/r;
+a2(:,2) = (a2(:,2)-min_val)/r;
 
 %seperate the audio files by size
 long = a1;
@@ -24,6 +34,8 @@ end
 
 [cL, lags_L] = xcorr(long(:,1),short(:,1));
 [cR, lags_R] = xcorr(long(:,2),short(:,2));
+
+error = max(abs(cR))*1E-6;
 
 [na, match_i] = max(abs(cL));
 
@@ -43,7 +55,7 @@ for i = lag_match-r:lag_match+r
     end
 %check if the frequency is the same
     if max(dif(:,2)) < error
-        [na, t] = max(dif(:,2));
+        [na, t] = min(dif(:,2));
          T = dif(t,1);
          T0 = T/Fs;
         if i == 1
